bea4ccece2892c5ff7f1fa036c0cb01e58bdd0ca
Matteo Umatche <74019874+matteoumatche@users.noreply.github.com>
2024-07-05 18:28:42 +0200

    seconda modifica

--- sensori.cpp
+++ sensori.cpp
@@ -1,7 +1,8 @@
 #include <iostream>
 #include <random>
 #include <functional>
-using namespace std;
+#include <string>
+#include "math.h"
 
 //--------------------Sensore--------------------
 class Sensore {
@@ -44,27 +45,22 @@ public:
     
     bool isAttivo() const;
 
-    virtual void simulaMisura(){} ;
+    void simulaMisura() override;
     bool Misura(bool valoreReale);
 };
 
 Fotocellula::Fotocellula(unsigned int id, std::string nome) : Sensore(id,nome), attivo(false) {}
 
-//restituisce un true 70% delle volte
-void Fotocellula::simulaMisura() {
-       std::random_device rd;
-    std::mt19937 gen(rd());
-
-    // Genera un valore booleano con la probabilità specificata
-    attivo=  gen() < 0.7 * gen.max();
-}
-
 bool Fotocellula::isAttivo() const {
     return attivo;
 }
 
-Fotocellula::Misura(bool valoreReale) {
-    attivo=attivo&&simulaMisura();
+void Fotocellula::simulaMisura() { 
+    attivo=true;
+}
+
+bool Fotocellula::Misura(bool valoreReale) {
+    
 }
 
 
@@ -74,29 +70,52 @@ private:
     double valoreMaxVelocita;
     double tolleranzaGoniometro;
     double tolleranzaAnemometro;
+    double offset;
     std::pair<double, double> dato;
+
 public:
-    Vento(unsigned int id, std::string nome);
+
+    double limitaAngolo(double x) const {
+    x = fmod(x,360);
+    if (x < 0)
+        x += 360;
+    return x;
+}
+    Vento(unsigned int id, std::string n,double o);
+    Vento(unsigned int id, std::string n,double o=0, double max=30, double tollG=0.1, double tollA=0.5);
     //--------------------getter--------------------
     double getMaxVelocita() const;
-    double getoffset();
+    double getOffset() const;
     double getTolleranzaGoniometro() const;
     double getTolleranzaAnemometro() const;
     std::pair<double, double> getDato() const;
     //--------------------metodi--------------------
+    void setOffset(double offset);
     void simulaMisura();
     std::pair<double, double> Misura(std::pair<double, double> valoreReale);
 };
 
-Vento::Vento(unsigned int id, std::string nome) : Sensore(id, nome), valoreMaxVelocita(30), tolleranzaGoniometro(0.1), tolleranzaAnemometro(0.5), dato(std::make_pair(0, 0)) {}
+Vento::Vento(unsigned int id, std::string n,double o) : 
+    Sensore(id, n),
+    offset(limitaAngolo(o)),
+    valoreMaxVelocita(30), 
+    tolleranzaGoniometro(0.1), 
+    tolleranzaAnemometro(0.5), 
+    dato(std::make_pair(0, 0)) {}    
 
-double Vento::offset() {
-    // Calcola l'offset tra la velocità massima e il valore attuale
-    return valoreMaxVelocita - dato.first;
-}
+Vento::Vento(unsigned int id, std::string n,double o, double max, double tollG, double tollA) : 
+    Sensore(id, n), 
+    valoreMaxVelocita(max>0 ? max : 30), 
+    tolleranzaGoniometro(limitaAngolo(tollG)?limitaAngolo(tollG):0.1), 
+    tolleranzaAnemometro(tollA>0 ? tollA : 0.5), 
+    dato(std::make_pair(0, 0)) {
+        offset = std::round(limitaAngolo(o)*tolleranzaGoniometro) * tolleranzaGoniometro;
+    }
 
-void Vento::setMaxVelocita(double maxVelocita) {
-    valoreMaxVelocita = maxVelocita;
+
+
+double Vento::getOffset() const {
+    return offset;
 }
 
 double Vento::getMaxVelocita() const {
@@ -118,15 +137,18 @@ double Vento::getTolleranzaAnemometro() const {
 void Vento::simulaMisura() {
     std::random_device rd;
     std::mt19937 gen(rd());
-    std::uniform_real_distribution<> dis(0, valoreMaxVelocita);
-    double velocita = dis(gen);
-    double angolo = dis(gen);
+    std::uniform_real_distribution<> disv(0, valoreMaxVelocita);
+    std::uniform_real_distribution<> disa(0, 360);
+    double velocita = std::round(disv(gen)/tolleranzaAnemometro) * tolleranzaAnemometro;
+    double angolo = std::round(disa(gen)/tolleranzaGoniometro) * tolleranzaGoniometro;
     dato = std::make_pair(velocita, angolo);
 }
 
 std::pair<double, double> Vento::Misura(std::pair<double, double> valoreReale) {
-    double velocita = valoreReale.first + offset();
-    double angolo = valoreReale.second;
+    std::uniform_real_distribution<> disv(0, valoreMaxVelocita);
+    std::uniform_real_distribution<> disa(0, 360);
+    double velocita = std::round(disv(valoreReale)/tolleranzaAnemometro) * tolleranzaAnemometro;
+    double angolo = std::round(disa(valoreReale)/tolleranzaGoniometro) * tolleranzaGoniometro;
     dato = std::make_pair(velocita, angolo);
     return dato;
 }
@@ -228,20 +250,89 @@ private:
     std::pair<double,double> rangeOttimale;
     double tolleranza;
     double dato;
-    enum distribuzione{};
 public:
-    Umidita(unsigned int id, std::string nome, distribuzione dist);
-    ~Umidita();
-    Umidita(const Umidita&);
-    double getMin();
-    double getMax();
-    double getDato();
-    double getTolleranza();
-    bool outOfRange();
-    std::pair<double,double> simulaMisura();
+    Umidita(unsigned int id, std::string n);
+    Umidita(unsigned int id, std::string n, double min, double max, std::pair<double,double> range, double toll);
+    //--------------------getter--------------------
+    double getMin() const;
+    double getMax() const;
+    double getDato() const;
+    double getTolleranza() const;
+    //--------------------metodi--------------------
+    bool outOfRange() const;
+    void simulaMisura();
+    double Misura(double);
 };
 
-class TemPercepita : public Temperatura, public Umidita {
+Umidita::Umidita(unsigned int id, std::string n) : 
+    Sensore(id, n), 
+    valoreMin(0), 
+    valoreMax(30), 
+    rangeOttimale(std::make_pair(5, 100)), 
+    tolleranza(0.1), 
+    dato(0) {
+}
+Umidita::Umidita(unsigned int id, std::string n, double min, double max, std::pair<double,double> range, double toll) : 
+    Sensore(id, n), 
+    valoreMin(min>0 ? min : 0), 
+    valoreMax(max>0 && max>= 100 ? max : 30), 
+    rangeOttimale(range), 
+    tolleranza(toll>0 ? toll : 0.1), 
+    dato(0) {
+        //da inserire controllo su range , min e max
+    }
+
+
+double Umidita::getMin() const {
+    return valoreMin;
+}
+
+double Umidita::getMax() const {
+    return valoreMax;
+}
+
+double Umidita::getDato() const {
+    return dato;
+}
+
+double Umidita::getTolleranza() const {
+    return tolleranza;
+}
+
+bool Umidita::outOfRange() const {
+    return dato < rangeOttimale.first || dato > rangeOttimale.second;
+}
+
+void Umidita::simulaMisura() {
+    std::random_device rd;
+    std::mt19937 gen(rd());
+    std::uniform_real_distribution<> dis(valoreMin, valoreMax);
+    dato = std::round(dis(gen) / tolleranza) * tolleranza;
+}
+
+double Umidita::Misura(double valoreReale) {
+    std::uniform_real_distribution<> dis(valoreMin, valoreMax);
+    return std::round(dato = dis(valoreReale) / tolleranza) * tolleranza;
+}
+class TemPercepita {
 private:
 public:
 };
+
+int main() {
+    
+    Fotocellula f(2, "fotocellula");
+    Vento v(3, "vento");
+    Temperatura t(4, "temperatura");
+    Umidita u(5, "umidita");
+    
+    v.simulaMisura();
+    t.simulaMisura();
+    u.simulaMisura();
+
+    std::cout << "ID: " << f.getID() << " Nome: " << f.getNome() << f.isAttivo() << std::endl;
+    std::cout << "ID: " << v.getID() << " Nome: " << v.getNome() << std::endl;
+    std::cout << "ID: " << t.getID() << " Nome: " << t.getNome() << t.getDato()<< std::endl;
+    std::cout << "ID: " << u.getID() << " Nome: " << u.getNome() << u.getDato()<< std::endl;
+    return 0;
+}
