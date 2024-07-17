e7fb528d546c8704d51f358da3ba941847e5e4ad
Nevia23 <81696502+Nevia23@users.noreply.github.com>
2024-07-17 22:55:03 +0200

    riordino

---
+++ src/Controller/sensore.cpp
@@ -0,0 +1,49 @@
+#include <iostream>
+#include <random>
+#include <functional>
+#include <string>
+#include <QJsonObject>
+#include "math.h"
+
+//--------------------Sensore--------------------
+class Sensore {
+private:
+    unsigned int ID;
+    std::string nome;
+public:
+    Sensore(unsigned int id, std::string nome);
+    Sensore(const QJsonObject& json);
+    /*ho eliminato il distruttore e il costruttore di copia perchè vanno bene quelli standard*/
+
+    std::string getNome() const;
+    unsigned int getID() const;
+    void setNome(std::string n);
+    QJsonObject salva() const;
+
+    virtual void simulaMisura() = 0;
+
+    virtual std::ostream& operator<<(std::ostream& os) const = 0;
+};
+
+Sensore::Sensore(unsigned int id, std::string n) : ID(id), nome(n) {}
+
+Sensore::Sensore(const QJsonObject& json) : ID(json["ID"].toInt()), nome(json["Nome"].toString().toStdString()) {}
+
+std::string Sensore::getNome() const {
+    return nome;
+}
+
+unsigned int Sensore::getID() const {
+    return ID;
+}
+
+void Sensore::setNome(std::string n) {
+    nome=n;
+}
+
+QJsonObject Sensore::salva() const {
+    QJsonObject json;
+    json["ID"] = static_cast<int>(ID);
+    json["Nome"] = QString::fromStdString(nome);
+    return json;
+}
