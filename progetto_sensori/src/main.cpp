db557caeba5da3c7fde489b975575fd5ae70e1b1
Nevia23 <81696502+Nevia23@users.noreply.github.com>
2024-07-18 17:11:32 +0200

    robe

--- progetto_sensori/src/main.cpp
+++ progetto_sensori/src/main.cpp
@@ -1,14 +1,11 @@
-// Includes Qt header files
-# include < QApplication >
-# include < QLabel >
+#include <QApplication>
+#include <QPushButton>
 
-int main (int argc , char * argv []) {
-// Defines an application object
-QApplication app ( argc , argv ) ;
-// A piece of text
-QLabel hello (" Hello world !") ;
-// Tells Qt to actually show the label
-hello . show () ;
-// Runs application
-return app . exec () ;
+int main(int argc, char **argv) {
+    QApplication app(argc, argv);
+
+    QPushButton button("Hello World");
+    button.show();
+
+    return app.exec();
 }
