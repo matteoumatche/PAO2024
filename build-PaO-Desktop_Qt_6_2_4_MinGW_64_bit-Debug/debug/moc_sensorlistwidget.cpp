7785bc6ed0c1fb20c29313772267181feaa3e736
Nevia23 <81696502+Nevia23@users.noreply.github.com>
2024-08-15 10:17:17 +0200

    kjhk

--- build-PaO-Desktop_Qt_6_2_4_MinGW_64_bit-Debug/debug/moc_sensorlistwidget.cpp
+++ build-PaO-Desktop_Qt_6_2_4_MinGW_64_bit-Debug/debug/moc_sensorlistwidget.cpp
@@ -22,8 +22,8 @@ QT_BEGIN_MOC_NAMESPACE
 QT_WARNING_PUSH
 QT_WARNING_DISABLE_DEPRECATED
 struct qt_meta_stringdata_View__SensorListWidget_t {
-    const uint offsetsAndSize[16];
-    char stringdata0[103];
+    const uint offsetsAndSize[12];
+    char stringdata0[73];
 };
 #define QT_MOC_LITERAL(ofs, len) \
     uint(offsetof(qt_meta_stringdata_View__SensorListWidget_t, stringdata0) + ofs), len 
@@ -34,14 +34,11 @@ QT_MOC_LITERAL(23, 13), // "sensorClicked"
 QT_MOC_LITERAL(37, 0), // ""
 QT_MOC_LITERAL(38, 15), // "Model::Sensore*"
 QT_MOC_LITERAL(54, 7), // "sensore"
-QT_MOC_LITERAL(62, 10), // "updateList"
-QT_MOC_LITERAL(73, 7), // "clonato"
-QT_MOC_LITERAL(81, 21) // "sendRequestUpdateList"
+QT_MOC_LITERAL(62, 10) // "updateList"
 
     },
     "View::SensorListWidget\0sensorClicked\0"
-    "\0Model::Sensore*\0sensore\0updateList\0"
-    "clonato\0sendRequestUpdateList"
+    "\0Model::Sensore*\0sensore\0updateList"
 };
 #undef QT_MOC_LITERAL
 
@@ -51,27 +48,19 @@ static const uint qt_meta_data_View__SensorListWidget[] = {
       10,       // revision
        0,       // classname
        0,    0, // classinfo
-       4,   14, // methods
+       2,   14, // methods
        0,    0, // properties
        0,    0, // enums/sets
        0,    0, // constructors
        0,       // flags
-       3,       // signalCount
+       2,       // signalCount
 
  // signals: name, argc, parameters, tag, flags, initial metatype offsets
-       1,    1,   38,    2, 0x06,    1 /* Public */,
-       5,    0,   41,    2, 0x06,    3 /* Public */,
-       6,    1,   42,    2, 0x06,    4 /* Public */,
-
- // slots: name, argc, parameters, tag, flags, initial metatype offsets
-       7,    0,   45,    2, 0x08,    6 /* Private */,
+       1,    1,   26,    2, 0x06,    1 /* Public */,
+       5,    0,   29,    2, 0x06,    3 /* Public */,
 
  // signals: parameters
     QMetaType::Void, 0x80000000 | 3,    4,
-    QMetaType::Void,
-    QMetaType::Void, 0x80000000 | 3,    4,
-
- // slots: parameters
     QMetaType::Void,
 
        0        // eod
@@ -85,8 +74,6 @@ void View::SensorListWidget::qt_static_metacall(QObject *_o, QMetaObject::Call _
         switch (_id) {
         case 0: _t->sensorClicked((*reinterpret_cast< std::add_pointer_t<Model::Sensore*>>(_a[1]))); break;
         case 1: _t->updateList(); break;
-        case 2: _t->clonato((*reinterpret_cast< std::add_pointer_t<Model::Sensore*>>(_a[1]))); break;
-        case 3: _t->sendRequestUpdateList(); break;
         default: ;
         }
     } else if (_c == QMetaObject::IndexOfMethod) {
@@ -105,13 +92,6 @@ void View::SensorListWidget::qt_static_metacall(QObject *_o, QMetaObject::Call _
                 return;
             }
         }
-        {
-            using _t = void (SensorListWidget::*)(Model::Sensore * );
-            if (*reinterpret_cast<_t *>(_a[1]) == static_cast<_t>(&SensorListWidget::clonato)) {
-                *result = 2;
-                return;
-            }
-        }
     }
 }
 
@@ -122,8 +102,8 @@ const QMetaObject View::SensorListWidget::staticMetaObject = { {
     qt_static_metacall,
     nullptr,
 qt_incomplete_metaTypeArray<qt_meta_stringdata_View__SensorListWidget_t
-, QtPrivate::TypeAndForceComplete<SensorListWidget, std::true_type>, QtPrivate::TypeAndForceComplete<void, std::false_type>, QtPrivate::TypeAndForceComplete<Model::Sensore *, std::false_type>, QtPrivate::TypeAndForceComplete<void, std::false_type>, QtPrivate::TypeAndForceComplete<void, std::false_type>, QtPrivate::TypeAndForceComplete<Model::Sensore *, std::false_type>
-, QtPrivate::TypeAndForceComplete<void, std::false_type>
+, QtPrivate::TypeAndForceComplete<SensorListWidget, std::true_type>, QtPrivate::TypeAndForceComplete<void, std::false_type>, QtPrivate::TypeAndForceComplete<Model::Sensore *, std::false_type>, QtPrivate::TypeAndForceComplete<void, std::false_type>
+
 
 
 >,
@@ -150,13 +130,13 @@ int View::SensorListWidget::qt_metacall(QMetaObject::Call _c, int _id, void **_a
     if (_id < 0)
         return _id;
     if (_c == QMetaObject::InvokeMetaMethod) {
-        if (_id < 4)
+        if (_id < 2)
             qt_static_metacall(this, _c, _id, _a);
-        _id -= 4;
+        _id -= 2;
     } else if (_c == QMetaObject::RegisterMethodArgumentMetaType) {
-        if (_id < 4)
+        if (_id < 2)
             *reinterpret_cast<QMetaType *>(_a[0]) = QMetaType();
-        _id -= 4;
+        _id -= 2;
     }
     return _id;
 }
@@ -172,13 +152,6 @@ void View::SensorListWidget::sensorClicked(Model::Sensore * _t1)
 void View::SensorListWidget::updateList()
 {
     QMetaObject::activate(this, &staticMetaObject, 1, nullptr);
-}
-
-// SIGNAL 2
-void View::SensorListWidget::clonato(Model::Sensore * _t1)
-{
-    void *_a[] = { nullptr, const_cast<void*>(reinterpret_cast<const void*>(std::addressof(_t1))) };
-    QMetaObject::activate(this, &staticMetaObject, 2, _a);
 }
 QT_WARNING_POP
 QT_END_MOC_NAMESPACE
