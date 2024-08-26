50d1ba7274f120052a775043d5ab14c1364c39a5
Nevia23 <81696502+Nevia23@users.noreply.github.com>
2024-08-26 13:48:27 +0200

    Update widgettempercepita.h

--- PaO/src/View/widgettempercepita.h
+++ PaO/src/View/widgettempercepita.h
@@ -2,24 +2,14 @@
 #define WIDGETTEMPERCEPITA_H
 
 #include "widgetgrafico.h"
-<<<<<<< HEAD
 #include "../Model/Sensore.h"
-=======
-#include "src/Model/sensore.h"
-
->>>>>>> 0cfb30eb57fb2a5115f3011dd6bb8e125d12ce5b
 namespace View{
 
 class WidgetTempercepita : public WidgetGrafico{
 
 public:
     explicit WidgetTempercepita(Model::Sensore* s,QWidget *parent );
-<<<<<<< HEAD
     void simulazione(Model::Sensore*) override;
-=======
-    void updateData(/* data specific to temperature sensor */);
-
->>>>>>> 0cfb30eb57fb2a5115f3011dd6bb8e125d12ce5b
 };
 
 }
