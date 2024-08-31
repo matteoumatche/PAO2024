b1f0b3891b70f393d85f2fb345f85d1fb9af8cca
Matteo Umatche <74019874+matteoumatche@users.noreply.github.com>
2024-08-31 22:20:20 +0200

    spinbox modifica

--- PaO/src/View/editsensordialog.cpp
+++ PaO/src/View/editsensordialog.cpp
@@ -5,6 +5,7 @@
 
 #include <QFormLayout>
 #include <QDialogButtonBox>
+#include <QDoubleSpinBox>
 
 EditSensorDialog::EditSensorDialog(Model::Sensore* sensore, QWidget* parent)
     : QDialog(parent), originalSensor(sensore) {
@@ -19,16 +20,36 @@ EditSensorDialog::EditSensorDialog(Model::Sensore* sensore, QWidget* parent)
         QString key = QString::fromStdString(pair.first);
 
         if (!(key == "Angolo" || key == "Velocita" || key == "Umidita" || key == "Temperatura" || key =="Dato"|| key =="IndiceCalore"|| key =="Attivo")) {
-            QLineEdit* edit = new QLineEdit(QString::fromStdString(pair.second), this);
-            if (key == "ID" || key == "Tipo") {
-                edit->setReadOnly(true);
-                formLayout->addRow(key + "*:", edit);
+            if(key=="Tolleranza"|| key=="MaxVelocita" || key=="TolleranzaAnemometro" || key=="TolleranzaGoniometro" ||key=="Offset"){
+                QDoubleSpinBox* spinBox = new QDoubleSpinBox();
+                spinBox->setValue(QString::fromStdString(pair.second).toDouble());
+
+                if(key=="Tolleranza" || key=="TolleranzaAnemometro"){
+                    spinBox->setRange(0, 10);
+                    spinBox->setDecimals(3);
+                }else if(key=="TolleranzaAnemometro"){
+                    spinBox->setRange(0, 90);
+                    spinBox->setDecimals(3);
+                }else if(key=="MaxVelocita"){
+                spinBox->setRange(0, 1000);
+                spinBox->setDecimals(2);
+                }else if(key=="Offset"){
+                    spinBox->setRange(0, 180);
+                    spinBox->setDecimals(2);
+                }
+                formLayout->addRow(key + ":", spinBox);
+                editFields[pair.first] = spinBox;
             }else{
-                formLayout->addRow(key + ":", edit);
+                QLineEdit* edit = new QLineEdit(QString::fromStdString(pair.second));
+                if (key == "ID" || key == "Tipo") {
+                    edit->setReadOnly(true);
+                    formLayout->addRow(key + "*:", edit);
+                }else{
+                    formLayout->addRow(key + ":", edit);
+                }
+                editFields[pair.first] = edit;
             }
-            editFields[pair.first] = edit;
         }
-
     }
 
 
@@ -39,59 +60,22 @@ EditSensorDialog::EditSensorDialog(Model::Sensore* sensore, QWidget* parent)
     connect(buttonBox, &QDialogButtonBox::rejected, this, &EditSensorDialog::reject);
 }
 
-/*
-EditSensorDialog::EditSensorDialog(Model::Sensore* sensore, QWidget* parent)
-    : QDialog(parent), originalSensor(sensore) {
-
-    setWindowTitle("Modifica Sensore");
-
-    QFormLayout* formLayout = new QFormLayout(this);
-
-    std::map<std::string, std::string> info = sensore->getInfo();
-
-    for (auto& pair : info) {
-        QString key = QString::fromStdString(pair.first);
-
-        if (key == "Tipo" || key == "Nome" || key == "ID" ||key == "Dato" || key == "Angolo" || key == "Velocita" ||
-            key == "Temperatura" || key == "Umidità") {
-            // Non mostrare le chiavi specificate
-            continue;
-        }
-
-        if (key == "Attivo") {
-            // Usa una QComboBox per la chiave "Attivo"
-            QComboBox* comboBox = new QComboBox(this);
-            comboBox->addItem("true");
-            comboBox->addItem("false");
-            comboBox->setCurrentText(QString::fromStdString(pair.second));
-            formLayout->addRow(key + ":", comboBox);
-            editFields[pair.first] = comboBox;
-        } else {
-            // Usa una QLineEdit per le altre chiavi
-            QLineEdit* edit = new QLineEdit(QString::fromStdString(pair.second), this);
-
-            if (key == "ID" || key == "Tipo") {
-                edit->setReadOnly(true);
-            }
-
-            formLayout->addRow(key + ":", edit);
-            editFields[pair.first] = edit;
-        }
-    }
-
-    QDialogButtonBox* buttonBox = new QDialogButtonBox(QDialogButtonBox::Ok | QDialogButtonBox::Cancel, this);
-    formLayout->addWidget(buttonBox);
-
-    connect(buttonBox, &QDialogButtonBox::accepted, this, &EditSensorDialog::accept);
-    connect(buttonBox, &QDialogButtonBox::rejected, this, &EditSensorDialog::reject);
-}
-*/
 void EditSensorDialog::accept() {
     std::map<std::string, std::string> updatedInfo;
 
     for (const auto& pair : editFields) {
-        QLineEdit* edit = pair.second;
-        QString value = edit->text();
+        QWidget* widget = pair.second;
+        QString value;
+
+        // Verifica se il widget è un QDoubleSpinBox
+        if (QDoubleSpinBox* spinBox = qobject_cast<QDoubleSpinBox*>(widget)) {
+            value = QString::number(spinBox->value(), 'f', spinBox->decimals());
+        }
+        // Altrimenti, trattalo come QLineEdit
+        else if (QLineEdit* edit = qobject_cast<QLineEdit*>(widget)) {
+            value = edit->text();
+        }
+
         updatedInfo[pair.first] = value.toStdString();
     }
 
@@ -100,25 +84,3 @@ void EditSensorDialog::accept() {
     QDialog::accept();
 }
 
-/*
-void EditSensorDialog::accept() {
-    std::map<std::string, std::string> updatedInfo;
-
-    for (const auto& pair : editFields) {
-        QWidget* widget = pair.second;
-        QString value;
-
-        // Usa qobject_cast per determinare il tipo di widget e ottenere il valore
-        if (auto* lineEdit = qobject_cast<QLineEdit*>(widget)) {
-            value = lineEdit->text();
-        } else if (auto* comboBox = qobject_cast<QComboBox*>(widget)) {
-            value = comboBox->currentText();
-        }
-
-        updatedInfo[pair.first] = value.toStdString();
-    }
-
-    emit sensorModified(updatedInfo);
-    qDebug() << "emesso";
-    QDialog::accept();
-}*/
